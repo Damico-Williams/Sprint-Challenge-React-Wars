29b477bf19819380641fad5b3351a57cc9b4e81b
damico williams <damicowilliams@gmail.com>
2020-01-10 17:17:20 -0800

    second api call test new

--- starwars/src/components/StarInfo.js
+++ starwars/src/components/StarInfo.js
@@ -1,37 +1,37 @@
 import React from 'react';
 import styled from 'styled-components';
 
-
-const Flex = styled.div`
+const FlexBox = styled.div`
    display:flex;
    flex-direction: column;
    justify-content:center;
-   align-items:center;
-   align-content: space-around;
    margin: 1rem 1.5rem 1rem 1.5rem;
-   
-`;
+   align-content: space-around;
+   align-items:center;
+  `;
 
 const Color = styled.h1`
    font-family: 'audiowide';
    font-weight: 'bold';
    color:#4141D8;
-   font-size:3rem;
+   text-shadow: 5px 3px 5px red;
+   font-size:4rem;
 `;
 
 const Size = styled.h3`
-   font-size:2rem;
+   font-size:3rem;
 `;
 
 
 export const StarInfo = (props) => {
          
        return (  
-         <Flex>
+         <FlexBox>
              <Color>{props.name}</Color>
              <Size>Gender: {props.gender}</Size>
              <Size>Eye Color: {props.eyes}</Size>
-         </Flex>
+             <Size>{props.movie}</Size>
+         </FlexBox>
 
          
        )
